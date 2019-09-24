4197101fba74d56309f358133971668dee999d2e
Davinder Cheema <davinder.cheema@dowjones.com>
2019-09-24 14:05:10 +0100

    [CHORE] Styled menu

--- pages/london.js
+++ pages/london.js
@@ -10,23 +10,26 @@ import '../styles/london/london.scss';
 const London = () => (
   <div className="container">
     <nav className="navigation">
-        Side bar
+      <div className="brand">
+        <div className="brand__logo" />
+        <div className="brand__name">Financial News</div>
+      </div>
+      <ul className="navigation__menu">
+        <li className="menu__item"><a className="menu__link" href="/">Top Stories</a></li>
+        <li className="menu__item"><a className="menu__link" href="/">News</a></li>
+        <li className="menu__item"><a className="menu__link" href="/">Views</a></li>
+        <li className="menu__item"><a className="menu__link" href="/">People</a></li>
+        <li className="menu__item"><a className="menu__link" href="/">Brexit</a></li>
+        <li className="menu__item"><a className="menu__link" href="/">Events & Awards</a></li>
+      </ul>
     </nav>
     <div className="main__container">
-      <header className="main__container__header">
-        Ads
-      </header>
+      <header className="main__container__header">Ads</header>
       <main className="main">
-        <section className="main__body">
-            body
-        </section>
-        <aside className="main__right">
-            right rail
-        </aside>
+        <section className="main__body">body</section>
+        <aside className="main__right">right rail</aside>
       </main>
-      <footer>
-        footer
-      </footer>
+      <footer>footer</footer>
     </div>
   </div>
 );
