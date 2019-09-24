04f1063c213fd450f0c2509f029814dc6c204f41
Davinder Cheema <davinder.cheema@dowjones.com>
2019-09-24 14:26:56 +0100

    [CHORE] Basic Ads styles added

--- pages/london.js
+++ pages/london.js
@@ -15,16 +15,52 @@ const London = () => (
         <div className="brand__name">Financial News</div>
       </div>
       <ul className="navigation__menu">
-        <li className="menu__item"><a className="menu__link" href="/">Top Stories</a></li>
-        <li className="menu__item"><a className="menu__link" href="/">News</a></li>
-        <li className="menu__item"><a className="menu__link" href="/">Views</a></li>
-        <li className="menu__item"><a className="menu__link" href="/">People</a></li>
-        <li className="menu__item"><a className="menu__link" href="/">Brexit</a></li>
-        <li className="menu__item"><a className="menu__link" href="/">Events & Awards</a></li>
+        <li className="menu__item">
+          <a className="menu__link" href="/">
+            Top Stories
+          </a>
+        </li>
+        <li className="menu__item">
+          <a className="menu__link" href="/">
+            News
+          </a>
+        </li>
+        <li className="menu__item">
+          <a className="menu__link" href="/">
+            Views
+          </a>
+        </li>
+        <li className="menu__item">
+          <a className="menu__link" href="/">
+            People
+          </a>
+        </li>
+        <li className="menu__item">
+          <a className="menu__link" href="/">
+            Brexit
+          </a>
+        </li>
+        <li className="menu__item">
+          <a className="menu__link" href="/">
+            Events & Awards
+          </a>
+        </li>
       </ul>
     </nav>
     <div className="main__container">
-      <header className="main__container__header">Ads</header>
+      <header className="main__container__header">
+        <div className="register">
+          <div className="register__content">
+            <div>
+              <p>Become a member. Join to access the insight and analysis powering the financial sector.</p>
+            </div>
+            <div className="button__container">
+              <a className="button" href="/">Sign In</a>
+              <a className="button button--alternative" href="/">Subscribe</a>
+            </div>
+          </div>
+        </div>
+      </header>
       <main className="main">
         <section className="main__body">body</section>
         <aside className="main__right">right rail</aside>
